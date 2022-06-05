f6f9a52c3387e8d4389c4e221e984290bfa0ee3c
chloechochon <102471688+chloechochon@users.noreply.github.com>
2022-06-05 18:58:20 +0200

    Update Objetcompose.h

--- Objetcompose.h
+++ Objetcompose.h
@@ -12,18 +12,16 @@
 class Objetcompose : public Dessinable {
 	public:
 	//constructeur 
-	///probleme: mais alors dnas le constructeur ont y'auras un proble
-	Objetcompose()=default;
-		 Objetcompose (ObjetMobile& A, Obstacle& B, ChampForces& C) : Obj(), Obs (), champsf () { ajoute(A); ajoute(B); ajoute(C);} 
-		 Objetcompose (ObjetMobile& A, Obstacle& B) : Obj(), Obs (), champsf () { ajoute(A);  ajoute(B);} 
-		 Objetcompose (Obstacle& B, ChampForces& C) : Obj(), Obs (), champsf () { ajoute(B);  ajoute(C);}
-		 Objetcompose (ObjetMobile& A, ChampForces& C) : Obj(), Obs (), champsf () { ajoute(A);  ajoute(C);} 
+	Objetcompose() { a_ete_ajoute=false;}
+	Objetcompose (ObjetMobile& A, Obstacle& B, ChampForces& C) : Obj(), Obs (), champsf () { ajoute(A); ajoute(B); ajoute(C); a_ete_ajoute=false;} 
+	Objetcompose (ObjetMobile& A, Obstacle& B) : Obj(), Obs (), champsf () { ajoute(A);  ajoute(B); a_ete_ajoute=false;} 
+	Objetcompose (Obstacle& B, ChampForces& C) : Obj(), Obs (), champsf () { ajoute(B);  ajoute(C); a_ete_ajoute=false;}
+	Objetcompose (ObjetMobile& A, ChampForces& C) : Obj(), Obs (), champsf () { ajoute(A);  ajoute(C); a_ete_ajoute=false;} 
 	
-	/// attention est ce que ca s'ecrit comme ca ????
-	//Destructeur si la methode ajoute a (qui les detruit car elle a la priorité (c'est des unique ptr donc ils sont detruit)) n'a pas été utilié
+	//Destructeur si la methode ajoute a (qui les detruit car elle a la priorité (c'est des unique ptr donc ils sont detruits)) n'a pas été utilié
 	~Objetcompose() {
 		
-		//pour les objet mobiles:
+		//pour les objets mobiles:
 		if (a_ete_ajoute){
 			for (size_t i(0); i<Obj.size(); i++) {
 				delete Obj[i];
@@ -37,19 +35,11 @@ class Objetcompose : public Dessinable {
 				std::cout <<"les obstacles ont ete detruit"<< std::endl;}
 			}
 			
-		/*//pour les objetscomposes
-		if (true 
-		 ){
-			for (size_t i(0); i<Objcompo.size(); i++) {
-				delete Objcompo[i];
-				std::cout <<"les Objetcompose ont ete detruit"<<std::endl;}
-			}*/
-			
 		 //pour les chammps de force
 		if (a_ete_ajoute){
 			for (size_t i(0); i<champsf.size(); i++) {
 				delete champsf[i];
-				std::cout <<"les champs de force ont ete detruit"<<std::endl;}
+				std::cout <<"les champs de force ont ete detruits"<<std::endl;}
 			}
 			
 		 
@@ -58,34 +48,26 @@ class Objetcompose : public Dessinable {
 	
  
 
-	void ajoute (ObjetMobile const&);
-	void ajoute (Obstacle const&);
-	void ajoute (ChampForces const&);
+	void ajoute (ObjetMobile const&);	//ajoute un objetmobile (en le mettant sous forme de pointeur) au tableau Obj
+	void ajoute (Obstacle const&);		//ajoute un Obstacle au tableau Obs
+	void ajoute (ChampForces const&);	//ajoute un ChampForces au tableau des champsf
+	
 	//methodes 
-	void ajoute_a(Systeme& S);
-	//virtual void dessine_sur (SupportADessin& )	
+	void ajoute_a(Systeme& S);		
+	//virtual void dessine_sur (SupportADessin& ) = 0;
 	
 	protected:
 	std::vector<ObjetMobile*> Obj;
 	std::vector<Obstacle*> Obs;
 	std::vector<ChampForces*> champsf;
-	//changement dans la conceptio: si un objett composé est composé d'objets composee chaqu'un des elements de l'objet composé sera rajouter au tableau correpondant
-	bool a_ete_ajoute; //pour savoir si l'objet composé a été ajouter au systeme 
+	bool a_ete_ajoute; 			//pour savoir si l'objet composé a été ajouté au systeme 
 };
 
 
-class Ventilateur :public Objetcompose  {
+class Ventilateur : public Objetcompose  {
 	
 	public:
-	Ventilateur (Brique bri, Vent v) : Objetcompose(bri,v) {
-		
-		/*
-		//pour forcer que la normale a la brique dans la meme direction que la normale du vent (direction du vent)
-		if ((largeur^normal).norme()> 0.001) {
-		Vecteur a(!normal); //a prend la direction de normal
-			largeur= largeur - (largeur*a)*a;
-		}*/
-		} 
+	Ventilateur (Brique bri, Vent v) : Objetcompose(bri,v) {}	//On force la construction du Ventilateur à avoir uniquement une brique et un vent
 
 	// virtual void dessine_sur(SupportADessin& support) override;
 	Obstacle* getbrique() const;
