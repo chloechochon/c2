d065ea7afe630f3286c7cb608a1ad952d0366604
chloechochon <102471688+chloechochon@users.noreply.github.com>
2022-06-05 19:58:22 +0200

    Update obstacle.h

--- obstacle.h
+++ obstacle.h
@@ -7,18 +7,12 @@
 #include "Dessinable.h"
 #include "Systeme.h"
 #include "constantes.h"
-/*struct Face{
-    array<int,6> num{origine,origine,origine,origine+longueur*a,origine+largeur*a;
-    Vecteur coincoin;
-    Vecteur normal;
-    double l;
-    Vecteur ELg;
-    double largeur;
-    Vecteur ELg} */
+
+//Ce fichier regroupe les Obstacles, les Plans, les Briques
     
 class Obstacle : public Dessinable {
     public:    
-        // Constructeur
+    // Constructeur
     Obstacle(Vecteur ori)
      : obs_origine(ori) {}
     
@@ -28,21 +22,21 @@ class Obstacle : public Dessinable {
     Vecteur getobs_origine() const;
    
     
-    virtual Vecteur point_plus_proche(const ObjetMobile& M) = 0;
-    virtual double distance(const ObjetMobile&) =0;
-	void agit_sur(ObjetMobile& obj);
-	virtual void affiche() =0;
+    virtual Vecteur point_plus_proche(const ObjetMobile& M) = 0;	//pour determiner quel est le point appartenant a l obstacle duquel l objet mobile est le plus proche
+    virtual double distance(const ObjetMobile&) =0;		//determiner la distance entre l objet et l obstacle
+    void agit_sur(ObjetMobile& obj);				//pour gerer les chocs
+    virtual void affiche() =0;
 
     ///virtual void dessine_sur(SupportADessin& support) = 0;
 	
-	//pour la covariance
+    //pour la covariance
     virtual Obstacle* copie() const = 0;
 
-	void ajoute_a(Systeme& S);
+    void ajoute_a(Systeme& S);
 
 	
     protected:
-    Vecteur obs_origine;
+    Vecteur obs_origine;		//Un obstacle a une origine comme unique attribut
     
 };
 
@@ -55,7 +49,7 @@ class Plan :public Obstacle {
     : Obstacle(ori), dir1(di1), dir2(di2){}
     
     Vecteur point_plus_proche(const ObjetMobile& M);
-    Vecteur n() const;
+    Vecteur n() const;			//pour determiner le vecteur normal au plan
     virtual void affiche() override;
     virtual double distance(const ObjetMobile&) override; //distance de l'ObjetMobile par rapport au plan
 
@@ -66,7 +60,7 @@ class Plan :public Obstacle {
     virtual Plan* copie() const override;
 
     private:
-    Vecteur dir1;
+    Vecteur dir1;	//Un plan est defini par deux vecteurs non colineaires et un point (l origine heritee d obstacle)
     Vecteur dir2;
     
 };
@@ -79,19 +73,19 @@ class Brique : public Obstacle {
         Vecteur a(!longueur); //a prend seulement la direction de longueur
         Vecteur b(!largeur); //b prend seulement la direction de largeur
         if((b|a) > epsilon) {
-            largeur = largeur - (largeur|a)*a; /// peut etre a verifier avec la formule
+            largeur = largeur - (largeur|a)*a;	 //il faut que les deux vecteurs soient orthogonaux et on modifie largeur en consequence
             }
         }                                   
         
-        //methode
+    //methodes
     Vecteur getlongueur() const;
     Vecteur getlargeur() const;
     double gethauteur() const;
    
-    Vecteur point_plus_proche(const ObjetMobile& M) override;
+    Vecteur point_plus_proche(const ObjetMobile& M) override;	//point appartenant a la brique dont l objet est le plus proche
     Vecteur n() const;
-	virtual void affiche() override;
-	virtual double distance(const ObjetMobile&) override;	//distance de l'ObjetMobile par rapport à une brique
+    virtual void affiche() override;
+    virtual double distance(const ObjetMobile&) override;	//distance de l'ObjetMobile par rapport à une brique
 
 
 ///	virtual void dessine_sur(SupportADessin& support) override;
@@ -102,10 +96,12 @@ class Brique : public Obstacle {
   
     
     private:
-    // de plus il herite d'une origine
+    // de plus il herite d'une origine par Obstacle
     Vecteur longueur;
     Vecteur largeur;
     double hauteur;
 };
 
-Vecteur point_portion (ObjetMobile const& M, Vecteur dir1, Vecteur dir2, Vecteur point);
+Vecteur point_portion (ObjetMobile const& M, Vecteur dir1, Vecteur dir2, Vecteur point); 
+// permet d utiliser la fonction point_plus_proche de plan dans la methode de Brique,
+// en modelisant un plan a partir de vecteurs directeurs de chaque face de Brique.
