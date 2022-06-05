5c6abf513a573e83fde95bafcdc687826364d4f1
chloechochon <102471688+chloechochon@users.noreply.github.com>
2022-06-05 20:19:52 +0200

    Update ex_vecteur.h

--- ex_vecteur.h
+++ ex_vecteur.h
@@ -11,23 +11,15 @@ class Vecteur {
     
     public:
     // Comme demandé, on a créé un constructeur pour vecteur nul mmais on l'a au final supprimé:
-    // le constructeur pour vecteur lambda peut en effet confondre si la dimension du vecteur est de 1 car on l initialiserait comme Vecteur P (1);
-    // De plus on n en voyait pas l utilite, ayant deja une constante vecteur nul de trois dimensions (les dimensions ne risquant pas de depasser 3)
-    /*
-    //constructeur du vecteur nul
-    Vecteur (int dim) {
-        for(int i(0);i<dim;i++){
-            vecteur.push_back(0.0);
-        }
-    }
-    */
+    // on n en voyait pas l utilite, ayant deja une constante vecteur nul de trois dimensions (les dimensions ne risquant pas de depasser 3 dans ce projet)
+    
     //Constructeur vecteur
     Vecteur(std::initializer_list<double> const& data) : vecteur(data) {};
     
     // On surcharge les constructeurs afin de pouvoir initialiser des vecteurs qui pourront avoir différentes dimensions:  une, deux ou trois dimensions (un vecteur de deux dimentions est un vecteur de 3 dimention avec un 0 dans la dernier composante 
     Vecteur () {}
-	Vecteur (double x) {vecteur.push_back(x);}
-	Vecteur (double x, double y) { vecteur.push_back(x); vecteur.push_back(y);}
+    Vecteur (double x) {vecteur.push_back(x);}
+    Vecteur (double x, double y) { vecteur.push_back(x); vecteur.push_back(y);}
     Vecteur (double x, double y, double z) {vecteur.push_back(x); vecteur.push_back(y); vecteur.push_back(z);}
 
 
@@ -36,29 +28,15 @@ class Vecteur {
     void setvecteur(std::vector<double> vec);
     double getcomposante(size_t i) const;
 
-    size_t taille() const;
+    size_t taille() const;	//renvoie la taille (la dimension) du Vecteur
+    double norme() const;	//renvoie la norme du vecteur
+    double norme2() const;	//renvoie le carre de la norme
     
-    
-
-    double norme() const;
-    double norme2() const;
-    
-    void augmente(double aug);
-    void set_coord(size_t coord, double valeur);
+    void augmente(double aug);	//augmente la dimension du vecteur de 1 en placant en push_back la valeur donnee en argument
+    void set_coord(size_t coord, double valeur);	//modifie la ième coordonee du Vecteur avec la valeur donnee en argument
     void dimension( Vecteur autre); //pour savoir si les vecteurs sont de la meme taille
     
-  //Remplacés en opérateurs
-///    Vecteur addition (Vecteur autre) const;
-///    Vecteur soustraction (Vecteur autre) const;
-///    Vecteur oppose () const;
-///    Vecteur mult (double a) const;
-///    Vecteur prod_vect (Vecteur autre) const;
-///    Vecteur unitaire () const;
-///    void affiche() const;
-///    bool compare(Vecteur v) const;
-///  double prod_scal (Vecteur autre) const;
-    
-    //Opérateurs
+    //Surcharge d'opérateurs
     bool operator==(const Vecteur& v) const;
     Vecteur operator+=(const Vecteur& autre);
     Vecteur operator-=(const Vecteur& autre);
@@ -74,10 +52,10 @@ class Vecteur {
 
 };
 
-double carre(double a);
+double carre(double a);	//Pour simplifier le calcul de puissances en renvoyant la valeur au carré
  std::ostream& operator<<(std::ostream& sortie, Vecteur const& v);
- Vecteur operator*(double lambda, Vecteur const& V) ;
- double operator|(double lambda, const Vecteur&  autre) ;
+ Vecteur operator*(double lambda, Vecteur const& V) ;	// pour avoir les deux sens de multiplication entre un scalaire et un vecteur
+ double operator|(double lambda, const Vecteur&  autre) ; //pour avoir le produit scalaire avec un scalaire (attention a l ordre)
  
 
 
